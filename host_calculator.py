8ea150de9b0ef53bcef9c222cf967a7b42839b95
br1P <39799992+br1P@users.noreply.github.com>
2023-06-05 20:28:09 -0300

    Add files via upload

---
+++ host_calculator.py
@@ -0,0 +1,11 @@
+print('hola mundo')
+
+numero = input('Ingrese el valor despues del / :')
+numero = int (numero)
+print('numero ingresado',numero)
+numero2 = 32 - numero
+print('restando 32 queda: ',numero2)
+numero3 = 2**numero2
+print('elevado a la 2 es:' ,numero3)
+numero4= numero3 -2
+print('el resultado es' ,numero4 , 'host en la red disponibles')
